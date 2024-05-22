ac47abbc97c55e1708bd22c499a425875df452f3
Karan Gupta <d22124440@mytudublin.ie>
2024-05-22 23:55:29 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { Lexend_Deca } from "next/font/google";
 import NavBar from "../components/NavBar";
 import Footer from "../components/Footer";
 import BackToTop from "@/components/BackToTop";
+import { GoogleTagManager } from '@next/third-parties/google'
 
 const lexend = Lexend_Deca({
   subsets: ["latin"],
@@ -30,6 +31,7 @@ export default function RootLayout({
         <Footer />
         <BackToTop />
       </body>
+      <GoogleTagManager gtmId="G-HTX4SEJPM3" />
     </html>
   );
 }
