64315a15556a1d702bd0821fec428a8e5515ed8f
Karan Gupta <d22124440@mytudublin.ie>
2024-05-22 23:59:00 +0100

    Added G Tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,26 @@ import { Lexend_Deca } from "next/font/google";
 import NavBar from "../components/NavBar";
 import Footer from "../components/Footer";
 import BackToTop from "@/components/BackToTop";
-import { GoogleTagManager } from '@next/third-parties/google'
+
+import Script from "next/script";
+
+export const GoogleAnalyticsTracking = () => {
+  return (
+    <>
+      {/* Global site tag (gtag.js) - Google Analytics */}
+      <Script src="https://www.googletagmanager.com/gtag/js?id=G-HTX4SEJPM3" />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-HTX4SEJPM3');
+        `}
+      </Script>
+    </>
+ );
+}
 
 const lexend = Lexend_Deca({
   subsets: ["latin"],
@@ -25,13 +44,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" data-theme="corporate">
+      <GoogleAnalyticsTracking />
       <body className={lexend.className}>
         <NavBar />
         {children}
         <Footer />
         <BackToTop />
       </body>
-      <GoogleTagManager gtmId="G-HTX4SEJPM3" />
+      
     </html>
   );
 }
